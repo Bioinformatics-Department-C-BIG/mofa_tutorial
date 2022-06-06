62e2914bce66305763221cd6e84abb50cde72782
athienitie <athienitie@example.com>
2022-06-06 11:57:47 +0300

    installations

---
+++ installation.R
@@ -0,0 +1,48 @@
+install.packages("BiocManager", repos = "http://cran.us.r-project.org")
+
+BiocManager::install("mixOmics")
+install.packages("BiocManager")
+
+
+if (!requireNamespace("BiocManager", quietly = TRUE))
+  install.packages("BiocManager")
+
+BiocManager::install("MOFA2")
+
+BiocManager::install('Org.Mm.eg.db')
+
+
+
+install.packages(
+  "ggplot2",
+  repos = c("http://rstudio.org/_packages",
+            "http://cran.rstudio.com")
+)
+install.packages(
+  "dplyr",
+  repos = c("http://rstudio.org/_packages",
+            "http://cran.rstudio.com")
+)
+
+install.packages('iCluster')
+library("iCluster")
+
+BiocManager::install(c("iClusterPlus"))
+BiocManager::install(version = '3.15')
+BiocManager::install("iClusterPlus")
+
+
+BiocManager::install("GenomicRanges")
+BiocManager::install("Glimma")
+
+
+install.packages('gplots')
+install.packages('lattice')
+
+
+install.packages('iCluster')
+
+install.packages('EdgeR')
+
+
+

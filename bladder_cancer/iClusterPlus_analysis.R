02a6667ec61300d2527ba9ae9366c99c14ecde46
eathieniti <efi.athieniti@gmail.com>
2022-07-21 17:27:00 +0300

    added significant features number

--- bladder_cancer/iClusterPlus_analysis.R
+++ bladder_cancer/iClusterPlus_analysis.R
@@ -12,6 +12,12 @@ mode='gene'
 ######## model selection 
 files=list()
 
+mode='protein'
+n.lambda=13
+ng_p=round(5,2)
+ng_g=round(35,2)
+
+
 mode='gene'
 n.lambda=89
 ng_p=round(5,2)
@@ -24,7 +30,7 @@ n.lambda_list=c(144,34,89,233,377,89, 217,13)
 mode='multi'
 n.lambda=233
 ng_p=round(4,2)
-ng_g=round(35,2)
+ng_g=round(25,2)
 #param_str_icl<-paste0( 'DE_data','_', most_var, '_ng_g_', round(1/ng_g,2),'_ncomp_g_','_ng_p_', round(1/ng_p,2)  )
 param_str_icl<-paste0(  most_var, '_ng_g_', ng_g,'_ng_p_', ng_p  )
 
@@ -94,136 +100,136 @@ get_results_for_run<-function(n.lambda){
   colnames(clusters)=paste("K=",2:(length(output)+1),sep="")
   #write.table(clusters, file="clusterMembership.txt",sep='\t',quote=F)
   k=which.max(devRatMinBIC)
-  k=3
-  
-  best.cluster=clusters[,k]
-  best.fit=output[[k]]$fit[[which.min(BIC[,k])]] 
-  getwd()
-  icluster_out<-'bladder_cancer/iCluster/'
-  
-  
-  
-  
-  
-  #### Generate heatmap
-  
-  #truncate the values for a better image plot
-  mRNA.image=data$mRNA
-  mRNA.image[mRNA.image>10]=10
-  mRNA.image[mRNA.image< -1]= -1
-  prot.image=data$proteomics
-  prot.image[prot.image>10]=10
-  prot.image[prot.image< -1]= -1
-  
-  mRNA.image=data$mRNA
-  #head(summary(head(mRNA.image)))
-  #mRNA.image[mRNA.image>10]=10
-  #mRNA.image[mRNA.image< -1]= -1
-  prot.image=data$proteomics
-  #prot.image[prot.image>10]=10
-  #prot.image[prot.image< -1]= -1
-  
-  
-  ###### Feature Selection
-  sigfeatures=alist()
-  features=alist()
-  features[[1]] = colnames(data$mRNA)
-  features[[2]] = colnames(data$proteomics)
-  
-  if (mode=='gene'){
+  k=2
+  for (k in 2:nK){
+    best.cluster=clusters[,k]
+    best.fit=output[[k]]$fit[[which.min(BIC[,k])]] 
+    getwd()
+    icluster_out<-'bladder_cancer/iCluster/'
+    
+    
+    
+    
+    
+    #### Generate heatmap
+    
+    #truncate the values for a better image plot
+    mRNA.image=data$mRNA
+    mRNA.image[mRNA.image>10]=10
+    mRNA.image[mRNA.image< -1]= -1
+    prot.image=data$proteomics
+    prot.image[prot.image>10]=10
+    prot.image[prot.image< -1]= -1
+    
+    mRNA.image=data$mRNA
+    #head(summary(head(mRNA.image)))
+    #mRNA.image[mRNA.image>10]=10
+    #mRNA.image[mRNA.image< -1]= -1
+    prot.image=data$proteomics
+    #prot.image[prot.image>10]=10
+    #prot.image[prot.image< -1]= -1
+    
+    
+    ###### Feature Selection
+    sigfeatures=alist()
+    features=alist()
     features[[1]] = colnames(data$mRNA)
+    features[[2]] = colnames(data$proteomics)
     
-  }else if (mode=='prot'){
-    features[[1]] = colnames(data$proteomics)
+    if (mode=='gene'){
+      features[[1]] = colnames(data$mRNA)
+      
+    }else if (mode=='prot'){
+      features[[1]] = colnames(data$proteomics)
+      
+    }
     
-  }
-  
-  
-  for(i in 1:length(best.fit$beta)){
-    rowsum=apply(abs(best.fit$beta[[i]]),1, sum)
-    upper=quantile(rowsum,prob=0.75)
-    sigfeatures[[i]]=(features[[i]])[which(rowsum>upper)]
+    upper_quantile=0.9
+    for(i in 1:length(best.fit$beta)){
+      rowsum=apply(abs(best.fit$beta[[i]]),1, sum)
+      upper=quantile(rowsum,prob=upper_quantile)
+      sigfeatures[[i]]=(features[[i]])[which(rowsum>upper)]
+      
+    }
     
-  }
-  
-  
-  #print a few examples of selected features
-  
-
-  if (length(sigfeatures)>1){
-    names(sigfeatures)=c("expression", "proteomics")
     
-    write.csv(sigfeatures[[2]], paste0(icluster_out,'Vars_proteins',param_str_icl, '_X','.csv'))
+    #print a few examples of selected features
     
-  }
-
   
-  head(sigfeatures[[1]], 15)
-  sigfeatures[[1]]
-  sigfeatures[[1]] %in% colnames(data$mRNA)
-
+    if (length(sigfeatures)>1){
+      names(sigfeatures)=c("expression", "proteomics")
+      
+      write.csv(sigfeatures[[2]], paste0(icluster_out,'Vars_proteins',param_str_icl, '_X','.csv'))
+      
+    }
   
-  ## PROBLEM HERE
+    
+    head(sigfeatures[[1]], 15)
+    sigfeatures[[1]]
+    sigfeatures[[1]] %in% colnames(data$mRNA)
   
-  bw.col = colorpanel(2,low="white",high="black")
-  col.scheme = alist()
-  col.scheme[[1]] = bluered(256)
-  col.scheme[[2]] = bluered(256)
-  
-  
-  if (mode == 'gene'){
-    fname = paste0(icluster_out,'Vars_genes',param_str_icl, '_X_', k ,n.lambda)
-    write.csv(sigfeatures[[1]], paste0(fname, '.csv' ))
-    top_genes=sigfeatures[[1]]
-    top_genes
-    gene_clusters=best.cluster
+    
+    ## PROBLEM HERE
+    
+    bw.col = colorpanel(2,low="white",high="black")
+    col.scheme = alist()
+    col.scheme[[1]] = bluered(256)
+    col.scheme[[2]] = bluered(256)
+    
+    
+    if (mode == 'gene'){
+      fname = paste0(icluster_out,'Vars_genes',param_str_icl, '_X_', k ,n.lambda)
+      write.csv(sigfeatures[[1]], paste0(fname, '_', upper_quantile,'.csv' ))
+      top_genes=sigfeatures[[1]]
+      top_genes
+      gene_clusters=best.cluster
+      write.csv(best.cluster, paste0(fname,  'best_clusters.csv'))
+      
+      png(paste0(icluster_out,param_str_icl,'_k_', k, '_' ,n.lambda, '_heatmap_genes.png'))
+      plotHeatmap(fit=best.fit,datasets=list(mRNA.image),
+                  type=c("gaussian","gaussian"), col.scheme = col.scheme,
+                  row.order=c(T,T),sparse=c(T,T),
+                  cap=c(F,F))
+      dev.off()
+    }
+    
+    if (mode=='prot'){
+      fname = paste0(icluster_out,'Vars_prot',param_str_icl, '_X_', k, n.lambda)
+      write.csv(sigfeatures[[1]], paste0(fname, '_', upper_quantile, '.csv' ))
+      write.csv(best.cluster, paste0(fname,  'best_clusters.csv'))
+      top_proteins=sigfeatures[[1]]
+      protein_clusters=best.cluster
+      
+      
+      png(paste0(icluster_out,param_str_icl,'_k_', k, '_' , n.lambda, '_heatmap_proteins.png'))
+      plotHeatmap(fit=best.fit,datasets=list(prot.image),
+                  type=c("gaussian","gaussian"), col.scheme = col.scheme,
+                  row.order=c(T,T),sparse=c(T,T),
+                  cap=c(F,F))
+      dev.off()
+      
+    }
+    
+    
+    
+    if (mode=='multi'){
+    fname = paste0(icluster_out,'Vars_multi',param_str_icl, '_X_', k,  n.lambda)
+    write.csv(sigfeatures[[1]], paste0(fname,'_', upper_quantile, '.csv' ))
     write.csv(best.cluster, paste0(fname,  'best_clusters.csv'))
-    
-    png(paste0(icluster_out,param_str_icl,'_k_', k, '_' ,n.lambda, '_heatmap_genes.png'))
-    plotHeatmap(fit=best.fit,datasets=list(mRNA.image),
-                type=c("gaussian","gaussian"), col.scheme = col.scheme,
-                row.order=c(T,T),sparse=c(T,T),
-                cap=c(F,F))
-    dev.off()
-  }
-  
-  if (mode=='prot'){
-    fname = paste0(icluster_out,'Vars_prot',param_str_icl, '_X_', k, n.lambda)
-    write.csv(sigfeatures[[1]], paste0(fname, '.csv' ))
-    write.csv(best.cluster, paste0(fname,  'best_clusters.csv'))
-    top_proteins=sigfeatures[[1]]
-    protein_clusters=best.cluster
+    multi_clusters=best.cluster
     
     
-    png(paste0(icluster_out,param_str_icl,'_k_', k, '_' , n.lambda, '_heatmap_proteins.png'))
-    plotHeatmap(fit=best.fit,datasets=list(prot.image),
-                type=c("gaussian","gaussian"), col.scheme = col.scheme,
-                row.order=c(T,T),sparse=c(T,T),
-                cap=c(F,F))
-    dev.off()
+   # png(paste0(icluster_out,param_str_icl, 'heatmap_multi_trained.png'))
+   # plotHeatmap(fit=best.fit,datasets=list(mRNA.image,prot.image),
+  #              type=c("gaussian","gaussian"), col.scheme = col.scheme,
+  #              row.order=c(T,T),sparse=c(T,T),
+  #              cap=c(F,F))
+  #  dev.off()
+    
+    
+    } 
     
   }
-  
-  
-  
-  if (mode=='multi'){
-  fname = paste0(icluster_out,'Vars_multi',param_str_icl, '_X_', k,  n.lambda)
-  write.csv(sigfeatures[[1]], paste0(fname, '.csv' ))
-  write.csv(best.cluster, paste0(fname,  'best_clusters.csv'))
-  multi_clusters=best.cluster
-  
-  
-  png(paste0(icluster_out,param_str_icl, 'heatmap_multi_trained.png'))
-  plotHeatmap(fit=best.fit,datasets=list(mRNA.image,prot.image),
-              type=c("gaussian","gaussian"), col.scheme = col.scheme,
-              row.order=c(T,T),sparse=c(T,T),
-              cap=c(F,F))
-  dev.off()
-  
-  
-  } 
-  
-  
   
   
   

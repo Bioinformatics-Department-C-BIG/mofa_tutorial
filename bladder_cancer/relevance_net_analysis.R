6049395b3bbf3b8cdf0a1c15f99dfc9857372b19
eathieniti <efi.athieniti@gmail.com>
2022-12-20 17:00:06 +0200

    relevance network visualization

---
+++ bladder_cancer/relevance_net_analysis.R
@@ -0,0 +1,10 @@
+
+library(igraph)
+cor_net<-correlation_net$gR
+cor_net
+is_bipartite(cor_net)
+
+## TODO: negative and positive weights--> how to plot? just make them different color/attribute
+plot(cor_net, 
+     arrow.mode=0,
+     vertex.size=20)

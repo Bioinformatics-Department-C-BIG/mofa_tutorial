b1f4463253b108703692846333d576d8da8b5c8e
eathieniti <efi.athieniti@gmail.com>
2023-04-15 16:33:38 +0300

    Find overlapping pathways

--- ppmi/Venn_comparisons.R
+++ ppmi/Venn_comparisons.R
@@ -9,17 +9,24 @@ library('VennDiagram')
 library(grid)
 source(paste0(script_dir, '/utils.R'))
 
-### Table of samples from all visits 
+process_mirnas=FALSE
 
+### Table of samples from all visits 
+#### FOR EACH MODALITY SEAPARETELY 
 out_compare<-'ppmi/plots/single/compare/'
+
+### this sets the outdirectory too
+source(paste0(script_dir, '/config.R'))
+
+
 dir.create(out_compare)
 
 log2fol_T<-0.1;padj_T<-.05;
 
 ### TODO: filter by threshold here!! 
 signif_file<-paste0('/significant', padj_T, '_',log2fol_T, '.csv')
-  
-  
+
+
 #### Firstly print numbers of samples 
 visits<-c('BL', 'V04', 'V06', 'V08')
 all_vs_ps<-filter_se(se,visits, sel_coh )
@@ -60,7 +67,7 @@ if  (process_mirnas){
   outdir_all<-paste0(outdir_orig, '/single/', 'mirnas_',visits, '_',MIN_COUNT_M, '_coh_',sel_coh_s, '_',des)
   title_x='mirna'
 }else{
-  outdir_all<-paste0(outdir_orig, '/single/',  'rnas_',visits, '_',MIN_COUNT_M, '_coh_',sel_coh_s, '_', des)
+  outdir_all<-paste0(outdir_orig, '/single/',  'rnas_',visits, '_',MIN_COUNT_G, '_coh_',sel_coh_s, '_', des)
   title_x='rna'
   
 }
@@ -70,11 +77,11 @@ all_visits<-lapply(outdir_all, function(x) {
   
   outfile<-paste0(x, signif_file)
   if (file.exists(outfile)){
-  return(as.data.frame(read.csv(outfile)))
+    return(as.data.frame(read.csv(outfile)))
   }else{
-      print(paste0(outfile,' not available'))
-    }
-  })
+    print(paste0(outfile,' not available'))
+  }
+})
 
 
 all_visits
@@ -83,11 +90,11 @@ log2fol_T<-0.1
 list_of_mirs<-lapply(all_visits,function(df)
   if ('X' %in% colnames(df)){
     df$sign_lfc2 <- ifelse(df$padj <padj_T & abs(df$log2FoldChange) >log2fol_T , TRUE, FALSE);
-  return(df[df$sign_lfc2,]$X)
+    return(df[df$sign_lfc2,]$X)
   }
 )
-list_of_mirs[[1]]
-merge(list_of_mirs, by='feats',all=TRUE)
+#list_of_mirs[[1]]
+#merge(list_of_mirs, by='feats',all=TRUE)
 
 
 listInput <- list(BL = list_of_mirs[[1]],
@@ -111,6 +118,57 @@ dev.off()
 calculate.overlap(listInput)
 
 venn.diagram(listInput,   
-             filename = paste0(out_compare,'14_venn_diagramm.png'), output=TRUE)
+             filename = paste0(out_compare,prefix,'14_venn_diagramm.png'), output=TRUE)
+
+
+
+
+############################# COMPARE PATHWAYS ###################################
+##################################################################################
+# for each visit across ALL modalities 
+VISIT='V08'
+outdir_mirs<-paste0(outdir_orig, '/single/', 'mirnas_',VISIT, '_',MIN_COUNT_M, '_coh_',sel_coh_s, '_',des)
+outdir_rnas<-paste0(outdir_orig, '/single/', 'rnas_',VISIT, '_',MIN_COUNT_G, '_coh_',sel_coh_s, '_',des)
+outdir_proteins<-paste0(outdir_orig, '/single/proteomics_', VISIT,'_coh_', sel_coh_s, '_', des, '/' )
+run_anova=TRUE
+
+outdir_s_p_enrich_file_ora<-paste0(outdir_proteins, '/enrichment/', 'BP_ora_T_0.05_anova_', run_anova)
+
+
+padj_paths<-0.05
+enrich_rnas_file<-paste0(results_file, '.csv')
+enrich_mirnas_file<-paste0(outdir_mirs,  '/enrichment/mirs_enrich__1_0_log2pval_GO Biological process (miRPathDB)',  '.csv')
+
+enrich_proteins_file<-paste0(outdir_s_p_enrich_file_ora,'.csv')
+enrich_rna<-read.csv(enrich_rnas_file)
+enrich_mirnas<-read.csv(enrich_mirnas_file)
+enrich_mirnas$Subcategory
+enrich_proteins<-read.csv(enrich_proteins_file)
+
+
+
+enrich_rna_sig<-enrich_rna[enrich_rna$p.adjust<padj_paths,]
+enrich_mirnas_sig<-enrich_mirnas[enrich_mirnas$P.adjusted<padj_paths,]
+
+enrich_proteins_sig<-enrich_proteins[enrich_proteins$p.adjust<padj_paths,]
+
+common_paths<-intersect(enrich_rna_sig$Description,enrich_proteins_sig$Description )
+
+
+listInput_all_mods<-list(rna=enrich_rna_sig$Description,
+                         prot=enrich_proteins_sig$Description, 
+                         mirnas=enrich_mirnas_sig$Subcategory)
+
+
+
+listInput<-listInput_all_mods
+res_overlap<-calculate.overlap(listInput)
+
+intersection_all_three<-Reduce(intersect,listInput_all_mods)
+write.csv(intersection_all_three, paste0(out_compare,'interesction_pathways.csv' ), row.names = FALSE)
+
+venn.diagram(listInput,   
+             filename = paste0(out_compare,'all_modalities_', padj_paths,  '_p_anova_',run_anova,'venn_diagramm.png'), output=TRUE)
+
 
 

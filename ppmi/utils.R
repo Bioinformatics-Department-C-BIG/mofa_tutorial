dc4b863fe00b62c92a51a9541dca91793feb8b7a
eathieniti <efi.athieniti@gmail.com>
2023-11-06 12:01:31 +0200

    added missing function

--- ppmi/utils.R
+++ ppmi/utils.R
@@ -610,6 +610,30 @@ run_ora_gene_list<-function(gene_list_ord, results_file){
 
 
 
+write_filter_gse_results<-function(gse_full,results_file,pvalueCutoff  ){
+  
+  ### Takes the full gse results, ie. without threshold significance, 
+  # saves it, 
+  # filters it by pvalueCutoff_sig
+  # and saves the filter 
+  #' @param gse_full full gse results objects 
+  #' @param results_file the file name to write results  (without .csv)
+  #' @param pvalueCutoff the pvalue used to obtain the gse results 
+  
+  write.csv(as.data.frame(gse_full@result), paste0(results_file, pvalueCutoff, '.csv'))
+  pvalueCutoff_sig<-0.05
+  gse_sig_result<-gse_full@result[gse_full@result$pvalue<pvalueCutoff_sig,]
+  write.csv(as.data.frame(gse_sig_result), paste0(results_file, pvalueCutoff_sig, '.csv'))
+  
+  # rewrite
+  dim(gse_full); dim(gse_sig_result)
+  ## filter gse result to significant only 
+  gse=dplyr::filter(gse_full, p.adjust < pvalueCutoff_sig)
+  return(gse)
+}
+
+
+
 
 
 #gse=gse_mofa_rna

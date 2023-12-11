02f1c371d83aea14b235a206e96b023bf0259cb6
eathieniti <efi.athieniti@gmail.com>
2023-12-11 19:33:57 +0200

    Update run_clustering_pipeline.R

--- ppmi/clustering/run_clustering_pipeline.R
+++ ppmi/clustering/run_clustering_pipeline.R
@@ -1,7 +1,12 @@
 
+script_dir<-paste0(dirname(rstudioapi::getSourceEditorContext()$path), '/../')
+script_dir
+source(paste0('ppmi/setup_os.R'))
+source(paste0(script_dir, 'ppmi/setup_os.R'))
 
 source(paste0(script_dir, 'ppmi/mofa_application_ppmi_all_visits.R'))
 source(paste0(script_dir, 'ppmi/mofa_analysis_time_diff.R'))
 source(paste0(script_dir, 'ppmi/clustering/mofa_clustering_analysis.R'))
 
 
+source(paste0(script_dir, 'ppmi/clustering/cluster_comparisons.R'))
